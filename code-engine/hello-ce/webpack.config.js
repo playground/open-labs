8426119f00fafd9042b15094702a84cd01f7b3eb
Jeff Lu <ljeff@us.ibm.com>
2022-05-13 19:09:45 -0400

    add code engine

---
+++ code-engine/hello-ce/webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const fs = require('fs');
+const CopyPlugin = require('copy-webpack-plugin');
+const { TsConfigPathsPlugin } = require('awesome-typescript-loader');
+const { ContextReplacementPlugin, IgnorePlugin } = require('webpack');
+const dist = 'dist';  // be aware 'dist' folder is also used for tsconfig output
+
+var nodeModules = {};
+// fs.readdirSync('node_modules')
+//   .filter(function(x) {
+//     return ['.bin'].indexOf(x) === -1;
+//   })
+//   .forEach(function(mod) {
+//     nodeModules[mod] = 'commonjs ' + mod;
+//   });
+
+module.exports = {
+  entry: {
+    'hello-ce': `./src/server.ts`
+  },
+  output: {
+    path: path.resolve(__dirname, dist),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: true
+          }
+        },
+        exclude: /node_modules/
+      }
+    ],
+  },
+  plugins: [
+    new CopyPlugin([
+    ])
+  ],
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx', '.json'],
+    plugins: [
+      new TsConfigPathsPlugin({configFile: './tsconfig.json'})
+    ],
+    alias: {
+      '@common/*': '../common/src'
+    }
+  },
+  externals: nodeModules,
+  mode: 'production',
+  target: 'node',
+  node: {
+    __dirname: true
+  }
+}
